2f7ee7d9c19315c8b72f2d062c24b2e84660cdfd
topKim <bighit.top@gmail.com>
2022-07-25 16:28:07 +0900

    lifting state up

--- src/index.js
+++ src/index.js
@@ -5,98 +5,23 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 
-// JSX
-/* import Library from './jsx/Library';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Library />
-  </React.StrictMode>
-); */
-
-
-// rendering elemnets
-/* import Clock from './rendering_elements/Clock';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-setInterval(() => {
-  root.render(
-    <React.StrictMode>
-      <Clock />
-    </React.StrictMode>
-  );
-}, 1000); */
-
-
-//component props
-/* import CommentList from './component_props/CommentList';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <CommentList />
-  </React.StrictMode>
-); */
-
-
-//state lifecycle
-/* import NotificationList from './state_lifecycle/NotificationList';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <NotificationList />
-  </React.StrictMode>
-); */
-
-//hooks
-/* import Accommodate from './hooks/Accommodate';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Accommodate />
-  </React.StrictMode>
-); */
-
-//handling events - class component
-/* import ConfirmButton_class from './handling_events/ConfirmButton_class';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <ConfirmButton_class />
-  </React.StrictMode>
-); */
-
-//handling events - function component
-/* import ConfirmButton_function from './handling_events/ConfirmButton_function';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <ConfirmButton_function />
-  </React.StrictMode>
-); */
-
-//conditional rendering
-/* import LandingPage from './conditional_rendering/LandingPage'
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <LandingPage />
-  </React.StrictMode>
-); */
-
-//list key
-/* import AttenanceBook from './list_key/AttendanceBook';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <AttenanceBook />
-  </React.StrictMode>
-); */
-
-//forms
+import Library from './jsx/Library';
+import Clock from './rendering_elements/Clock';
+import CommentList from './component_props/CommentList';
+import NotificationList from './state_lifecycle/NotificationList';
+import Accommodate from './hooks/Accommodate';
+import ConfirmButton_class from './handling_events/ConfirmButton_class';
+import ConfirmButton_function from './handling_events/ConfirmButton_function';
+import LandingPage from './conditional_rendering/LandingPage'
+import AttenanceBook from './list_key/AttendanceBook';
 import SignUp from './forms/SignUp';
+import Calculator from './lifting_state_up/Calculator';
+
+//lifting state up
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <SignUp />
+    <Calculator />
   </React.StrictMode>
 );
 
